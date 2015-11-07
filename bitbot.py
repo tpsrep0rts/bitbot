f74249b01729cffc005ead40ecfd7354ae03f140
tpsrep0rts <brian@zanimation.com>
2015-11-06 23:50:17 -0800

    cleanup output

--- bitbot.py
+++ bitbot.py
@@ -8,9 +8,10 @@ class BitBot:
   """Bitcoin trading bot"""
 
   BITSTAMP_URL = 'http://www.bitstamp.net/api/ticker/'
+  SECONDS_PER_MINUTE = 60
   SECONDS_PER_HOUR = 3600
   REFRESH_INTERVAL = 5
-
+  QUERY_INTERVAL = 300
   DROP_TABLE_QUERY = "DROP TABLE bitcoin_prices;"
   CREATE_TABLE_QUERY = "CREATE TABLE bitcoin_prices(id INTEGER PRIMARY KEY AUTOINCREMENT, quote_time INTEGER , price FLOAT);"
 
@@ -29,10 +30,9 @@ class BitBot:
     self.c.execute('SELECT * FROM bitcoin_prices WHERE quote_time >= {min_quote_time} AND quote_time <= {max_quote_time}'.\
         format(min_quote_time=min_time, max_quote_time=max_time))
     all_rows = self.c.fetchall()
-    print "Price\tTime"
     for row in all_rows:
       date_string = datetime.datetime.fromtimestamp(row[1]).strftime('%Y-%m-%d %H:%M:%S')
-      print str(row[2]) + "\t" + date_string
+      print "{:.2f}".format(row[2])  + "\t" + date_string
 
   def insert(self, price, time):
     query = "INSERT INTO bitcoin_prices (quote_time, price) VALUES ('{quote_time}', '{quote_price}');"\
@@ -45,11 +45,16 @@ class BitBot:
     return priceFloat
 
   def start(self):
+    print "Price\tTime"
+    current_time = int(time.time())
+    self.query_db(current_time-self.QUERY_INTERVAL,current_time)
     while True:
       try:
         current_price = self.query_bitstamp()
-        self.insert(current_price, int(time.time()))
-        self.query_db(time.time() - self.SECONDS_PER_HOUR, time.time())
+        current_time = int(time.time())
+        self.insert(current_price, current_time)
+        date_string = datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
+        print "{:.2f}".format(current_price)  + "\t" + date_string
       except requests.ConnectionError:
         print "Error querying Bitstamp API"
       time.sleep(self.REFRESH_INTERVAL)
