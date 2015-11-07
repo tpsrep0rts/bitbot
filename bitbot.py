ed1106e31c3078bcc5251ee20fbe487bca97d4f0
tpsrep0rts <brian@zanimation.com>
2015-11-07 03:22:31 -0800

    bitcoin_traders

--- bitbot.py
+++ bitbot.py
@@ -4,6 +4,7 @@ import time
 import datetime
 import requests,json
 import utils
+from bitcoin_trader import *
 import warnings
 
 class BitBot:
@@ -12,7 +13,7 @@ class BitBot:
   BITSTAMP_URL = 'http://www.bitstamp.net/api/ticker/'
   SECONDS_PER_MINUTE = 60
   SECONDS_PER_HOUR = 3600
-  REFRESH_INTERVAL = 5
+  REFRESH_INTERVAL = 1
   QUERY_INTERVAL = 300
   DROP_TABLE_QUERY = "DROP TABLE bitcoin_prices;"
   CREATE_TABLE_QUERY = "CREATE TABLE bitcoin_prices(id INTEGER PRIMARY KEY AUTOINCREMENT, quote_time INTEGER , price FLOAT);"
@@ -38,18 +39,28 @@ class BitBot:
       warnings.simplefilter("ignore")
       r = requests.get(self.BITSTAMP_URL)
     priceFloat = float(json.loads(r.text)['last'])
+    TraderManager.add_bitcoin_data(priceFloat)
     return priceFloat
 
   def query_db(self, min_time, max_time):
-    self.c.execute('SELECT * FROM bitcoin_prices WHERE quote_time >= {min_quote_time} AND quote_time <= {max_quote_time}'.\
+    self.c.execute('SELECT quote_time, price FROM bitcoin_prices WHERE quote_time >= {min_quote_time} AND quote_time <= {max_quote_time}'.\
         format(min_quote_time=min_time, max_quote_time=max_time))
     return self.c.fetchall()
 
   def print_db_results(self, db_results):
     for row in db_results:
-      date_string = datetime.datetime.fromtimestamp(row[1]).strftime('%Y-%m-%d %H:%M:%S')
-      self.last_price = "{:.2f}".format(row[2])
+      date_string = datetime.datetime.fromtimestamp(row[0]).strftime('%Y-%m-%d %H:%M:%S')
+      self.last_price = self.format_dollars(row[1])
       print self.last_price + "\t" + date_string
+
+  def format_dollars(self, dollars):
+    return  "{:.2f}".format(dollars)
+
+  def register_traders(self, db_results):
+    TraderManager.add_trader(ContentTrader(db_results))
+    TraderManager.add_trader(BullTrader(db_results))
+    TraderManager.add_trader(BearTrader(db_results))
+    TraderManager.add_trader(HoldUntilDecline(db_results))
 
   def start(self):
     self.last_price = "0.00"
@@ -57,20 +68,31 @@ class BitBot:
     current_time = int(time.time())
     db_results = self.query_db(current_time - self.QUERY_INTERVAL, current_time)
     self.print_db_results(db_results)
+    self.register_traders(db_results)
 
     while True:
-      try:
-        current_price = "{:.2f}".format(self.query_bitstamp())
-        current_time = int(time.time())
-        date_string = datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
-        print current_price  + "\t" + date_string
+      self.on_awake()
 
-        if(self.last_price != current_price):
-          self.insert(current_price, current_time)
-          self.last_price = current_price
-      except requests.ConnectionError:
-        print "Error querying Bitstamp API"
-      time.sleep(self.REFRESH_INTERVAL)
+  def on_awake(self):
+    try:
+      current_price =self.format_dollars(self.query_bitstamp())
+      TraderManager.add_bitcoin_data(current_price)
+
+
+      current_time = int(time.time())
+      date_string = datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
+
+
+      recommendations = TraderManager.compute_recommended_actions()
+
+      print current_price  + "\t" + date_string + " Recommendations: " + ",".join(recommendations)
+
+      if(self.last_price != current_price):
+        self.insert(current_price, current_time)
+        self.last_price = current_price
+    except requests.ConnectionError:
+      print "Error querying Bitstamp API"
+    time.sleep(self.REFRESH_INTERVAL)
 
   def __del__(self):
     self.conn.commit()
