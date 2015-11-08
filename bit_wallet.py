555ad00153ff22d7ac3c90b49bc6ad7b4cb4d131
CorySvensson <Cory.Svensson@gmail.com>
2015-11-08 11:10:11 -0800

    some bug, but some work done too

--- bit_wallet.py
+++ bit_wallet.py
@@ -16,7 +16,7 @@ class BitWallet:
     try:
       self.c.execute(self.CREATE_TABLE_QUERY)
     except sqlite3.OperationalError as e:
-      print "sqlite3 error: " + str(e)
+      print "sqlite3 error: " + str(e)      
 
   def query_db(self):
     self.c.execute("SELECT * FROM investments")
@@ -26,14 +26,18 @@ class BitWallet:
     self.dollars += dollars
 
   def purchase_bitcoin(self, bitcoin_qty, bitcoin_price):
-    dollar_cost = bitcoin_qty * bitcoin_price
+    dollar_cost = bitcoin_qty * bitcoin_price *-1
     self.dollars -= dollar_cost
-    self.bitcoin_records.append( [bitcoin_qty, bitcoin_price, dollar_cost * -1, int(time.time())] )
+    ptime = int(time.time())
+    self.bitcoin_records.append( [bitcoin_qty, bitcoin_price, dollar_cost, ptime] )
+    query = "INSERT INTO investments (qty, price, standing, purchase_time) VALUES ('{bitcoin_qty}', '{bitcoin_price}', '{dollar_cost}', '{ptime}');"\
+        .format(bitcoin_qty=bitcoin_qty, bitcoin_price=bitcoin_price, dollar_cost=dollar_cost, ptime = ptime)
+    self.c.execute(query)
 
   def convert_bitcoin_by_index(self, index, bitcoin_pct, bitcoin_price):
     amount = self.bitcoin_records[index][0] * bitcoin_pct * bitcoin_price
     self.dollars += amount
-    self.bitcoin_records[index][2] += amount
+    self.bitcoin_records[index][2] = self.bitcoin_records[index][2] + amount
     self.bitcoin_records[index][0] = self.bitcoin_records[index][0] - self.bitcoin_records[index][0] * bitcoin_pct;
 
     if bitcoin_pct == 1.0:
@@ -42,23 +46,28 @@ class BitWallet:
   def __str__(self):
     return "Records: " + str(self.bitcoin_records) + "\nDollars:" + str(self.dollars)
 
+  def __del__(self):
+    self.conn.commit()
+    self.conn.close()
+
 
 wallet = BitWallet(1000.00)
-
+wallet.initialize_db()
+wallet.query_db()
 wallet.add_dollars(500.00)
 
-wallet.purchase_bitcoin(1.5, 420)
-wallet.purchase_bitcoin(.5, 420)
+wallet.purchase_bitcoin(1.5, 420.00)
+wallet.purchase_bitcoin(0.5, 420.00)
 
 
 print wallet
 
 
-wallet.convert_bitcoin_by_index(0,.5,500)
+wallet.convert_bitcoin_by_index(0.0,0.5,500.00)
 print wallet
 
-wallet.convert_bitcoin_by_index(0,.5,500)
+wallet.convert_bitcoin_by_index(0.0,0.5,500.00)
 print wallet
 
-wallet.convert_bitcoin_by_index(0,1.0,500)
+wallet.convert_bitcoin_by_index(0.0,1.0,500.00)
 print wallet
